195eb5b7584d9bdc44c343d39a537c2605d9a6b3
dqhieu <dqhieu13@gmail.com>
2023-04-06 13:49:36 +0700

    Add profile photo

--- HieuDinhTheApp/ContentView.swift
+++ HieuDinhTheApp/ContentView.swift
@@ -7,20 +7,82 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+struct ProfilePhotoView: View {
+
+  @State private var isRotating = 0.0
+  @State private var scale = 0.0
+
+  var body: some View {
+    ZStack {
+      ZStack {
+        Color
+          .yellow
+          .frame(width: 100, height: 100)
+        //          .cornerRadius(20)
+          .blur(radius: 50)
+          .offset(x: -20, y: -20)
+        Color
+          .red
+          .frame(width: 100, height: 100)
+        //          .cornerRadius(20)
+          .blur(radius: 50)
+          .offset(x: -20, y: 20)
+        Color
+          .purple
+          .frame(width: 100, height: 100)
+        //          .cornerRadius(20)
+          .blur(radius: 50)
+          .offset(x: 20, y: 20)
+        Color
+          .blue
+          .frame(width: 100, height: 100)
+        //          .cornerRadius(20)
+          .blur(radius: 50)
+          .offset(x: 20, y: -20)
+      }
+      //      .cornerRadius(80)
+      .rotationEffect(.degrees(isRotating))
+      .onAppear {
+        withAnimation(.linear
+          .speed(0.05).repeatForever(autoreverses: false)) {
+            isRotating = 720.0
+          }
+      }
+      Image("profile")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 200, height: 200)
+        .cornerRadius(100)
     }
+    .scaleEffect(scale)
+    .onAppear {
+      withAnimation(.spring()) {
+        scale = 1
+      }
+    }
+    .padding()
+  }
+}
+
+struct ContentView: View {
+
+  @State var id = UUID()
+
+  var body: some View {
+    VStack {
+      ProfilePhotoView()
+        .onTapGesture {
+          id = UUID()
+          UIImpactFeedbackGenerator(style: .soft).impactOccurred()
+        }
+    }
+    .padding()
+    .id(id)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }

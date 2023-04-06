cb69ce36ebc047115c4d942e0fa43fb9ce0ab5ba
Hieu Dinh <dqhieu13@gmail.com>
2023-04-06 13:07:05 +0700

    Initial commit

---
+++ HieuDinhTheApp/HieuDinhTheAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HieuDinhTheAppApp.swift
+//  HieuDinhTheApp
+//
+//  Created by Dinh Quang Hieu on 06/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct HieuDinhTheAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
